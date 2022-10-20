a9483abee35d4ee3566e47f7b1b244f97b7497b8
SushantaNandy <nandysushanta8@gmail.com>
2022-10-20 11:38:59 +0530

    moolya java class for opps

---
+++ com/company/var_args.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class var_args {
+    static  int values(int x,int ...arr) {  // this method is created so to get free of the hussle of creating multiple argument
+        int sum = 0;                  // u can make a bar, by atleast passing one sureshot parameter
+        for (int a : arr) {
+            sum =sum+ a;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(values(1,2));   // you can pass n number of argument
+    }
+}
